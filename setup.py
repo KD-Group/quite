fd194d771aa03a2170a14bb0b808185c45025b90
SF-Zhou <sfzhou.scut@gmail.com>
2018-01-14 19:00:01 +0800

    bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quite',
-    version='0.1.6',
+    version='0.1.7',
     description='QT UI Extension',
     url='https://github.com/sf-zhou/quite',
 
