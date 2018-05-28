c3b27ec6d0ba1d138a13c4af22ce2501c0737c2c
Jefung <jefung865424525@gmail.com>
2018-05-28 11:35:47 +0800

    add cannot_closed signal into closeEvent

--- quite/gui/widgets/dialog.py
+++ quite/gui/widgets/dialog.py
@@ -1,17 +1,25 @@
-from .. import QDialog
-from .. import QCloseEvent
-from .. import ui_extension
 from .. import ClosedSignalInterface, ClassExecInterface, ContainerAbilityInterface
+from .. import EventLoop
+from .. import QCloseEvent
+from .. import QDialog
+from .. import ui_extension
 
 
 @ui_extension
 class Dialog(QDialog, ClosedSignalInterface, ClassExecInterface, ContainerAbilityInterface):
-    def closeEvent(self, event: QCloseEvent):
-        self.closed.emit()
-        event.accept()
 
-    def exec(self, *args):
-        super().exec_(*args)
+    def closeEvent(self, event: QCloseEvent):
+        if self.can_close:
+            self.closed.emit()
+            event.accept()
+        else:
+            self.cannot_closed.emit()
+            event.ignore()
+
+    def exec(self):
+        with EventLoop() as event:
+            self.show()
+            self.closed.connect(event.quit)
 
     @property
     def size(self) -> (int, int):
