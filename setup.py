acafcf5af901497b21fb0444afe15bb334b03154
Jefung <Jefung@users.noreply.github.com>
2019-07-27 19:15:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quite',
-    version='0.2.2',
+    version='0.2.3',
     description='QT UI Extension',
     url='https://github.com/sf-zhou/quite',
 
