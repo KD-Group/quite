cdf6097b8fc37116ece1d1808c7d2ca84015b75d
Jefung <jefung865424525@gmail.com>
2019-08-30 15:22:57 +0800

    auto use tag as version, add readme

--- setup.py
+++ setup.py
@@ -9,12 +9,65 @@ https://github.com/sf-zhou/quite
 """
 
 from setuptools import setup, find_packages
+import os
+import subprocess
+
+
+def get_git_latest_tag():
+    def _minimal_ext_cmd(cmd: str):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd("git describe --abbrev=0 --tags")
+        git_tag = out.strip().decode('ascii')
+    except OSError:
+        git_tag = None
+
+    return git_tag
+
+
+latest_tag = get_git_latest_tag()
+if latest_tag is None:
+    print("get_git_latest_tag return None")
+    exit(1)
+
+# 读取项目下的requirements.txt
+req_txt_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+if os.path.exists(req_txt_path):
+    with open(req_txt_path) as f:
+        req_list = f.readlines()
+    req_list = [x.strip() for x in req_list]
+else:
+    req_list = []
+
+
+def readme():
+    """读取README.md文件"""
+    with open('README.md', encoding="utf-8") as f:
+        return f.read()
+
+
+print("use latest tag as version: {}".format(latest_tag))
+print("use requirements.txt as install_requires: {}".format(req_list))
 
 setup(
     name='quite',
-    version='0.2.3',
+    version=latest_tag,
     description='QT UI Extension',
-    url='https://github.com/sf-zhou/quite',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/KD-Group/quite',
 
     author='SF-Zhou',
     author_email='sfzhou.scut@gmail.com',
@@ -33,5 +86,5 @@ setup(
 
     keywords='qt ui',
     packages=find_packages(exclude=['docs', 'tests', 'examples']),
-    install_requires=['st', 'prett', 'typing']
+    install_requires=req_list,
 )
