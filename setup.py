200ccdce13ffc92833cbd8014100dce2aca2bca0
Jefung <jefung865424525@gmail.com>
2019-09-01 21:45:56 +0800

    fix setup.py error

--- setup.py
+++ setup.py
@@ -9,41 +9,8 @@ https://github.com/sf-zhou/quite
 """
 
 from setuptools import setup, find_packages
+from version import get_git_version
 import os
-import subprocess
-
-
-def get_git_latest_tag():
-    def _minimal_ext_cmd(cmd: str):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd("git describe --abbrev=0 --tags")
-        git_tag = out.strip().decode('ascii')
-        # 去除tag中的v/V
-        if str(git_tag).startswith("v") or str(git_tag).startswith("V"):
-            git_tag = str(git_tag)[1:]
-    except OSError:
-        git_tag = None
-
-    return git_tag
-
-
-latest_tag = get_git_latest_tag()
-if latest_tag is None:
-    print("get_git_latest_tag return None")
-    exit(1)
 
 # 读取项目下的requirements.txt
 req_txt_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
@@ -61,12 +28,12 @@ def readme():
         return f.read()
 
 
-print("use latest tag as version: {}".format(latest_tag))
+print("use latest tag as version: {}".format(get_git_version()))
 print("use requirements.txt as install_requires: {}".format(req_list))
 
 setup(
     name='quite',
-    version=latest_tag,
+    version=get_git_version(),
     description='QT UI Extension',
     long_description=readme(),
     long_description_content_type='text/markdown',
@@ -89,5 +56,6 @@ setup(
 
     keywords='qt ui',
     packages=find_packages(exclude=['docs', 'tests', 'examples']),
+    include_package_data=True,
     install_requires=req_list,
 )
